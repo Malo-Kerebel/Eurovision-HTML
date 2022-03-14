df48a291791fb05da5ce41d8a694518ea52e2a2e
Malo Kerebel <malo.kerebel@laposte.net>
2022-03-14 11:08:35 +0100

    fixed bash script

--- eurovision.sh
+++ eurovision.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
 output=""
-echo $(echo "bon jour" | tr -d ' ')
 
 for i in $(seq 1 $(wc -l countries.txt | awk '{print $1}'))
 do
     url=$(cat url.txt | head -n $i | tail -n 1)
-    output+=$(wget -qO- $url | grep -oP '(?<={"text":" • "},{"text":").*(?=vue)' | tr -d ' '),
+    output+=$(wget -qO- $url | grep -oP '(?<={"text":" • "},{"text":").*(?=vue)' | tr -d ' ' | tr -d ' '),
 done
 echo $output >> eurovision.csv
