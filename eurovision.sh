55aa1326738a39ce4dcfbf172b2433972148305e
Malo Kerebel <malo.kerebel@laposte.net>
2022-03-16 20:36:30 +0100

    Fixed bug from spanish lyrics

--- eurovision.sh
+++ eurovision.sh
@@ -5,6 +5,6 @@ output=""
 for i in $(seq 1 $(wc -l countries.txt | awk '{print $1}'))
 do
     url=$(cat url.txt | head -n $i | tail -n 1)
-    output+=$(wget -qO- $url | grep -oP '(?<={"text":" • "},{"text":").*(?=vue)' | tr -d ' ' | tr -d ' '),
+    output+=$(wget -qO- $url | grep -oP '(?<={"text":" • "},{"text":").*(?=vues)' | tr -d ' ' | tr -d ' '),
 done
 echo $output >> eurovision.csv
